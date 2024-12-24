b1faf1c943e498e244bd8991f747827880b3d212
searica <143636061+searica@users.noreply.github.com>
2024-12-23 16:55:40 -0800

    Initial commit

---
+++ OpenSesame.cs
@@ -0,0 +1,192 @@
+﻿// Ignore Spelling: CameraTweaks Jotunn
+
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+using System.Reflection;
+using UnityEngine;
+using System;
+
+namespace OpenSesame;
+
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+internal sealed class OpenSesame : BaseUnityPlugin
+{
+    internal const string Author = "Searica";
+    public const string PluginName = "OpenSesame";
+    public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
+    public const string PluginVersion = "0.1.0";
+
+
+
+    public void Awake()
+    {
+        Log.Init(Logger);
+
+
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
+        Game.isModded = true;
+
+    }
+
+    public void OnDestroy()
+    {
+    }
+}
+
+
+[HarmonyPatch]
+internal static class DoorPatches
+{
+    /// <summary>
+    ///     Claim ownership before trying to open a door.
+    /// </summary>
+    /// <param name="__instance"></param>
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Door), nameof(Door.Open))]
+    public static void Door_Open_Prefix(Door __instance)
+    {
+        if (!__instance || !__instance.m_nview || !__instance.m_nview.IsValid())
+        {
+            return;
+        }
+        __instance.m_nview.ClaimOwnership();
+    }
+}
+
+/// <summary>
+///     Log level to control output to BepInEx log
+/// </summary>
+internal enum LogLevel
+{
+    Low = 0,
+    Medium = 1,
+    High = 2,
+}
+
+/// <summary>
+///     Helper class for properly logging from static contexts.
+/// </summary>
+internal static class Log
+{
+    #region Verbosity
+
+    internal static ConfigEntry<LogLevel> Verbosity { get; set; }
+    internal static LogLevel VerbosityLevel => Verbosity.Value;
+    internal static bool IsVerbosityLow => Verbosity.Value >= LogLevel.Low;
+    internal static bool IsVerbosityMedium => Verbosity.Value >= LogLevel.Medium;
+    internal static bool IsVerbosityHigh => Verbosity.Value >= LogLevel.High;
+
+    #endregion Verbosity
+
+    private static ManualLogSource logSource;
+
+    internal static void Init(ManualLogSource logSource)
+    {
+        Log.logSource = logSource;
+    }
+
+    internal static void LogDebug(object data) => logSource.LogDebug(data);
+
+    internal static void LogError(object data) => logSource.LogError(data);
+
+    internal static void LogFatal(object data) => logSource.LogFatal(data);
+
+    internal static void LogMessage(object data) => logSource.LogMessage(data);
+
+    internal static void LogWarning(object data) => logSource.LogWarning(data);
+
+    internal static void LogInfo(object data, LogLevel level = LogLevel.Low)
+    {
+        if (Verbosity is null || VerbosityLevel >= level)
+        {
+            logSource.LogInfo(data);
+        }
+    }
+
+    internal static void LogGameObject(GameObject prefab, bool includeChildren = false)
+    {
+        LogInfo("***** " + prefab.name + " *****");
+        foreach (Component compo in prefab.GetComponents<Component>())
+        {
+            LogComponent(compo);
+        }
+
+        if (!includeChildren) { return; }
+
+        LogInfo("***** " + prefab.name + " (children) *****");
+        foreach (Transform child in prefab.transform)
+        {
+            if (!child) { continue; }
+
+            LogInfo($" - {child.name}");
+            foreach (Component compo in child.GetComponents<Component>())
+            {
+                LogComponent(compo);
+            }
+        }
+    }
+
+    internal static void LogComponent(Component compo)
+    {
+        if (!compo) { return; }
+        try
+        {
+            LogInfo($"--- {compo.GetType().Name}: {compo.name} ---");
+        }
+        catch (Exception ex)
+        {
+            Log.LogError(ex.ToString());
+            Log.LogWarning("Could not get type name for component!");
+            return;
+        }
+
+        try
+        {
+            PropertyInfo[] properties = compo.GetType().GetProperties(ReflectionUtils.AllBindings);
+            foreach (PropertyInfo property in properties)
+            {
+                try
+                {
+                    LogInfo($" - {property.Name} = {property.GetValue(compo)}");
+                }
+                catch (Exception ex)
+                {
+                    Log.LogError(ex.ToString());
+                    Log.LogWarning($"Could not get property: {property.Name} for component!");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.LogError(ex.ToString());
+            Log.LogWarning("Could not get properties for component!");
+        }
+
+        try
+        {
+            FieldInfo[] fields = compo.GetType().GetFields(ReflectionUtils.AllBindings);
+            foreach (FieldInfo field in fields)
+            {
+                try
+                {
+                    LogInfo($" - {field.Name} = {field.GetValue(compo)}");
+                }
+                catch (Exception ex)
+                {
+                    Log.LogError(ex.ToString());
+                    Log.LogWarning($"Could not get field: {field.Name} for component!");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.LogError(ex.ToString());
+            Log.LogWarning("Could not get fields for component!");
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,51 @@
+# DiscoveryPins
+Customizable pin colors and automatic pinning of things you discover and interact with. 
+Can be used as a purely client side mod or it can be installed on the server to sync some of the settings.
+
+## Features
+- Configure pin colors.
+- Automatically pin dungeons when you enter them (configurable).
+- Automatically pin ore when you damage it (configurable).
+- Automatically pin portals when building or interacting with them (configurable).
+	- If the portal has a tag then it names the pin after the tag.
+	- Updates pin names when you change the portal tag.
+	- Will fail to remove the old pin if you change the pin type for portals and then change the portal tag.
+- Automatically remove death pins when retrieving your tombstone (configurable).
+- Configure whether to generate death pins when inventory is empty.
+- Configurable keyboard shortcut to automatically pin things within your field of view or very close to your character that are configured to be automatically pinned.
+- Synced settings for whether the keyboard shortcut is enabled and how far away it can trigger automatic pins.
+
+## Donations/Tips
+My mods will always be free to use but if you feel like saying thanks you can tip/donate.
+
+| My Ko-fi: | [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/searica) |
+|-----------|---------------|
+
+## Source Code
+Source code is available on Github.
+
+| Github Repository: | <img height="18" src="https://github.githubassets.com/favicons/favicon-dark.svg"></img><a href="https://github.com/searica/DiscoveryPins"> DiscoveryPins</a> |
+|-----------|---------------|
+
+### Contributions
+If you would like to provide suggestions, make feature requests, or reports bugs and compatibility issues you can either open an issue on the Github repository or tag me (@searica) with a message on my discord [Searica's Mods](https://discord.gg/sFmGTBYN6n).
+
+### Credits
+This mod was inspired by QoLPins by Tekla.
+
+## Shameless Self Plug (Other Mods By Me)
+If you like this mod you might like some of my other ones.
+
+#### Building Mods
+- [Extra Snap Points Made Easy](https://thunderstore.io/c/valheim/p/Searica/Extra_Snap_Points_Made_Easy/)
+- [AdvancedTerrainModifiers](https://thunderstore.io/c/valheim/p/Searica/AdvancedTerrainModifiers/)
+- [BuildRestrictionTweaksSync](https://thunderstore.io/c/valheim/p/Searica/BuildRestrictionTweaksSync/)
+- [ToolTweaks](https://thunderstore.io/c/valheim/p/Searica/ToolTweaks/)
+
+#### Gameplay Mods
+- [CameraTweaks](https://thunderstore.io/c/valheim/p/Searica/CameraTweaks/)
+- [DodgeShortcut](https://thunderstore.io/c/valheim/p/Searica/DodgeShortcut/)
+- [FortifySkillsRedux](https://thunderstore.io/c/valheim/p/Searica/FortifySkillsRedux/)
+- [ProjectileTweaks](https://thunderstore.io/c/valheim/p/Searica/ProjectileTweaks/)
+- [SkilledCarryWeight](https://thunderstore.io/c/valheim/p/Searica/SkilledCarryWeight/)
+- [SafetyStatus](https://thunderstore.io/c/valheim/p/Searica/SafetyStatus/)
